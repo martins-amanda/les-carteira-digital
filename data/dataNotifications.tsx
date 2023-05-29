2559b0920eb3af8dfc0ee888bca6e497b5d96a74
Amanda Martins <amanda.a.martins@hotmail.com>
2023-05-29 01:48:18 -0300

    feat: mock data

---
+++ data/dataNotifications.tsx
@@ -0,0 +1,20 @@
+export const dataNotifications = [
+  {
+    id: '1',
+    title: 'Meta Alcançada!',
+    desc: 'Parabéns por ter alcançado a sua meta "Dinheiro extra" de: R$ 500,00.',
+    date: '1d',
+  },
+  {
+    id: '2',
+    title: 'A meta Alugel Casa está para vencer!',
+    desc: 'Você cadastrou a meta "Alugel Casa" no valor de R$ 500,00 com vencimento no dia 12/05/2023.',
+    date: '2d',
+  },
+  {
+    id: '3',
+    title: 'Pagar conta de Celular está para vencer!',
+    desc: 'Você cadastrou a meta "Pagar conta de Celular" no valor de 60,90 reais com vencimento no dia 28/09/2023.',
+    date: '15d',
+  },
+];

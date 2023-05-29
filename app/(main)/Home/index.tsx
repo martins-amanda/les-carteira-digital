a59c96dfbedf4fb0cc60b3bc6e9a555b1e6e93f6
Amanda Martins <amanda.a.martins@hotmail.com>
2023-05-29 01:46:00 -0300

    feat: home

--- app/(main)/Home/index.tsx
+++ app/(main)/Home/index.tsx
@@ -1,16 +1,33 @@
-import { Button } from '@components/Button/Button';
-import Dropdown from '@components/Dropdown/Dropdown';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Text } from 'react-native';
-import { GlobalContainer } from '@global/styles';
-import { Avatar, HomeContainer, WelcomeContainer, WelcomeText } from './styles';
+
+import { Divider, GlobalContainer } from '@global/styles';
+import { theme } from '@global/theme';
+import { ButtonOutline } from '@components/ButtonOutline/ButtonOutline';
+import { ScrollView, View } from 'react-native';
+import { TextButton } from '@components/TextButton/TextButton';
+import { useRouter } from 'expo-router';
+import { CardHistory } from '@components/CardHistory/CardHistory';
+import { FlatList } from 'react-native-gesture-handler';
+import { dataHistory } from 'data/dataHistory';
+import {
+  Avatar,
+  Button,
+  ButtonText,
+  MoneyContent,
+  Row,
+  Text,
+  WelcomeContainer,
+  WelcomeText,
+} from './styles';
 
 type FormData = {
   name: string;
 };
 
 const Home = () => {
+  const router = useRouter();
+
   const { control, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (data: FormData) => {
@@ -18,7 +35,7 @@ const Home = () => {
   };
 
   return (
-    <GlobalContainer>
+    <GlobalContainer style={{ justifyContent: 'flex-start' }}>
       <WelcomeContainer>
         <Avatar
           source="https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg"
@@ -27,6 +44,88 @@ const Home = () => {
         />
         <WelcomeText>Olá, Vini Shorts!</WelcomeText>
       </WelcomeContainer>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <MoneyContent>
+          <Text>Seu saldo atual é: </Text>
+          <Text
+            style={{
+              fontSize: 24,
+              fontFamily: theme.fonts.semibold,
+              marginTop: 15,
+            }}
+          >
+            R$ 25.500,00
+          </Text>
+          <Text style={{ fontSize: 12 }}>Última atualização: 12/05/2023</Text>
+        </MoneyContent>
+        <Button onPress={() => router.push('/NewTransaction')}>
+          <ButtonText>Nova tansação</ButtonText>
+        </Button>
+
+        <Divider style={{ marginVertical: 20 }} />
+
+        <MoneyContent>
+          <Row style={{ justifyContent: 'space-between' }}>
+            <Row>
+              <Text>Meta: </Text>
+              <Text style={{ fontFamily: theme.fonts.semibold }}>Viagem</Text>
+            </Row>
+
+            <Text style={{ fontSize: 12 }}>até: 12/12/2023</Text>
+          </Row>
+
+          <Row style={{ marginTop: 10, alignItems: 'center' }}>
+            <Text style={{ fontSize: 20 }}>R$ 9.000,00 </Text>
+            <Text style={{ fontSize: 18, color: theme.colors.secondary }}>
+              de R$ 10.000,00{' '}
+            </Text>
+          </Row>
+        </MoneyContent>
+
+        <ButtonOutline
+          style={{
+            width: 150,
+            marginTop: 20,
+            borderRadius: 15,
+            borderColor: theme.colors.secondary,
+          }}
+        >
+          Atualizar meta
+        </ButtonOutline>
+
+        <Divider style={{ marginVertical: 20 }} />
+
+        <Row style={{ justifyContent: 'space-between' }}>
+          <Text style={{ fontSize: 18, fontFamily: theme.fonts.medium }}>
+            Seu histórico:{' '}
+          </Text>
+          <TextButton fontSize={18} href="/History">
+            ver mais
+          </TextButton>
+        </Row>
+
+        <Divider style={{ marginVertical: 20 }} />
+        <FlatList
+          data={dataHistory.slice(0, 3)}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <View>
+              <CardHistory
+                data={item}
+                onPressEdit={() =>
+                  router.push({
+                    pathname: '/NewTransaction',
+                    params: { id: item.id },
+                  })
+                }
+              />
+              <Divider style={{ marginVertical: 20 }} />
+            </View>
+          )}
+          nestedScrollEnabled
+        />
+      </ScrollView>
     </GlobalContainer>
   );
 };
--- app/(main)/Home/styles.ts
+++ app/(main)/Home/styles.ts
@@ -16,6 +16,8 @@ export const Avatar = styled(Image)`
 export const WelcomeContainer = styled.View`
   flex-direction: row;
   align-items: center;
+  margin-top: 35px;
+  margin-bottom: 15px;
 `;
 export const WelcomeText = styled.Text`
   font-size: 16px;
@@ -23,4 +25,33 @@ export const WelcomeText = styled.Text`
   color: ${({ theme }) => theme.colors.text_white};
   text-align: center;
   margin-left: 15px;
+`;
+
+export const MoneyContent = styled.View``;
+export const Text = styled.Text`
+  font-size: 16px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.text_white};
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 150px;
+  min-height: 45px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 20px;
+  flex-direction: row;
+  margin-top: 20px;
+  margin-bottom: 12px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.text_black};
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;

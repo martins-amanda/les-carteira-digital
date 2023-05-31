5a443311cf625d7f594292b04308dc0e2e5e63b7
Amanda Martins <amanda.a.martins@hotmail.com>
2023-05-31 02:47:26 -0300

    fix: validations changes

--- validation/AuthLogin.validation.ts
+++ validation/AuthLogin.validation.ts
@@ -5,12 +5,12 @@ export type LoginForm = Yup.InferType<typeof LoginSchema>;
 
 export const LoginSchema = Yup.object({
   email: Yup.string()
-    .required('Preencha os campos necessários')
+    .required('Campo obrigatório')
     .email('E-mail Inválido')
     .trim(),
   password: Yup.string()
     .min(8, 'Insira uma senha válida')
-    .required('Preencha os campos necessários')
+    .required('Campo obrigatório')
     .trim(),
 });
 
@@ -18,13 +18,11 @@ export type SignupForm = Yup.InferType<typeof SignupSchema>;
 
 export const SignupSchema = Yup.object({
   name: Yup.string().required('Campo Obrigatório'),
-  email: Yup.string()
-    .required('Preencha os campos necessários')
-    .email('E-mail Inválido'),
+  email: Yup.string().required('Campo obrigatório').email('E-mail Inválido'),
 
   password: Yup.string()
     .min(8, 'Insira uma senha válida')
-    .required('Preencha os campos necessários')
+    .required('Campo obrigatório')
     .trim(),
 
   cpf: Yup.string().required('Campo Obrigatório'),
@@ -46,7 +44,7 @@ export type Forgot = Yup.InferType<typeof ForgotPasswordSchema>;
 
 export const ForgotPasswordSchema = Yup.object({
   email: Yup.string()
-    .required('Preencha os campos necessários')
+    .required('Campo obrigatório')
     .email('E-mail Inválido')
     .trim(),
 });
@@ -54,7 +52,7 @@ export const ForgotPasswordSchema = Yup.object({
 export type Transactions = Yup.InferType<typeof TransactionsSchema>;
 
 export const TransactionsSchema = Yup.object({
-  title: Yup.string().required('Preencha os campos necessários').trim(),
+  title: Yup.string().required('Campo obrigatório').trim(),
   value: Yup.string(),
 
   category: Yup.string(),
@@ -68,7 +66,47 @@ export const TransactionsSchema = Yup.object({
         return null;
       }
     })
-    .typeError('Data de nascimento inválida')
-    .min('01/01/1900', 'Data de nascimento inválida')
+    .typeError('Data inválida')
+    .min('01/01/1900', 'Data inválida')
+    .required('Data obrigatória'),
+});
+
+export type Goal = Yup.InferType<typeof GoalSchema>;
+
+export const GoalSchema = Yup.object({
+  title: Yup.string().required('Campo obrigatório').trim(),
+  value: Yup.string().required('Campo obrigatório'),
+
+  final_date: Yup.date()
+    .transform((value, originalValue) => {
+      try {
+        const NewDate = parse(originalValue, 'dd/MM/yyyy', new Date());
+        return NewDate;
+      } catch (error) {
+        return null;
+      }
+    })
+    .typeError('Data inválida')
+    .min('01/01/1900', 'Data inválida')
+    .required('Data obrigatória'),
+});
+
+export type GoalUpdate = Yup.InferType<typeof GoalUpdateSchema>;
+
+export const GoalUpdateSchema = Yup.object({
+  title: Yup.string().required('Campo obrigatório').trim(),
+  value: Yup.string().required('Campo obrigatório'),
+  total_raised: Yup.string().required('Campo obrigatório'),
+  final_date: Yup.date()
+    .transform((value, originalValue) => {
+      try {
+        const NewDate = parse(originalValue, 'dd/MM/yyyy', new Date());
+        return NewDate;
+      } catch (error) {
+        return null;
+      }
+    })
+    .typeError('Data inválida')
+    .min('01/01/1900', 'Data inválida')
     .required('Data obrigatória'),
 });
